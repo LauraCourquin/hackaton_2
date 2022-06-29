fe1b291b273b9d63bb31a4a920dd19c75e9158d0
NtGZeko <matheooz@live.fr>
2022-06-29 22:17:02 +0200

    create componants Card is functionnaly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import Card from "@components/Card";
+
 export default function App() {
-  return <div>App</div>;
+  return <Card />;
 }
---
+++ frontend/src/components/Card/index.jsx
@@ -0,0 +1,9 @@
+import SCard from "./style";
+
+export default function Card() {
+  return (
+    <SCard>
+      <div>index</div>
+    </SCard>
+  );
+}
