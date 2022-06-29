9de99a1c5a1355e85eabe9d20e998049e060a03b
NtGZeko <matheooz@live.fr>
2022-06-29 22:32:25 +0200

    add link img for test

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -3,7 +3,12 @@ import SCard from "./style";
 export default function Card() {
   return (
     <SCard>
-      <div>index</div>
+      <article className="myCard">
+        <img
+          src="https://www.netcost-security.fr/wp-content/uploads/2021/11/1636044306_Lhistoire-de-GigaChad-lultra-masculin-mi-meme-mi-legende-urbaine.jpg"
+          alt=""
+        />
+      </article>
     </SCard>
   );
 }
--- frontend/src/components/Card/style.jsx
+++ frontend/src/components/Card/style.jsx
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export default styled.section``;
+export default styled.section`
+  background-color: aquamarine;
+  width: 100vw;
+  min-height: 100vw;
+`;
