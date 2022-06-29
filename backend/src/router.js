958df5d6bb116c7ca5289d14aa87327810acbf4d
laura <lauracourquin@outlook.com>
2022-06-29 17:01:32 +0200

    Added router project and actor and modified db

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
-const { ItemController } = require("./controllers");
+const {
+  ItemController,
+  ActorController,
+  ProjectController,
+} = require("./controllers");
 
 const router = express.Router();
 
@@ -9,5 +13,8 @@ router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
+
+router.get("/actor", ActorController.browse);
+router.length("/project", ProjectController.browse);
 
 module.exports = router;
