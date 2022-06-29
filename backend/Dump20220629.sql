4a6e31d2b8d9c0336f0ca9384af0ac27de85b096
laura <lauracourquin@outlook.com>
2022-06-29 15:33:08 +0200

    Added bdd

---
+++ backend/Dump20220629.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE  IF NOT EXISTS `Hackathon2203` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `Hackathon2203`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: Hackathon2203
+-- ------------------------------------------------------
+-- Server version	8.0.29-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `acteur`
+--
+
+DROP TABLE IF EXISTS `acteur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `acteur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `picture` varchar(45) DEFAULT NULL,
+  `skills` json DEFAULT NULL,
+  `mail` varchar(255) NOT NULL,
+  `jobs` varchar(45) DEFAULT NULL,
+  `types` enum('client','collaborateur','prestateur externe','intervenant') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acteur`
+--
+
+LOCK TABLES `acteur` WRITE;
+/*!40000 ALTER TABLE `acteur` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acteur` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projet`
+--
+
+DROP TABLE IF EXISTS `projet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projet` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `inProject` tinyint NOT NULL DEFAULT '0',
+  `name` varchar(65) DEFAULT NULL,
+  `clientName` varchar(65) DEFAULT NULL,
+  `acteur_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_projet_acteur_idx` (`acteur_id`),
+  CONSTRAINT `fk_projet_acteur` FOREIGN KEY (`acteur_id`) REFERENCES `acteur` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projet`
+--
+
+LOCK TABLES `projet` WRITE;
+/*!40000 ALTER TABLE `projet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projet` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-29 15:28:09
