74b518c95a47f25e6b4263d8821575436a8fe46d
SDecambray <samir.decambray@gmail.com>
2022-06-30 09:49:52 +0200

    add button style and icon, add the button:active class, create the border

--- frontend/src/components/Sidebar/index.jsx
+++ frontend/src/components/Sidebar/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import logo from "@assets/SideBar/apside.png";
 import shop from "@assets/SideBar/shop.svg";
+import client from "@assets/SideBar/client.png";
+import partenaire from "@assets/SideBar/partenaire.png";
+import batiment from "@assets/SideBar/batiment.png";
+
 import SCardForm from "./style";
 
 export default function SideBar() {
@@ -8,13 +12,35 @@ export default function SideBar() {
     <SCardForm>
       <img src={logo} alt="Logo Apside" />
       <button type="button">
-        <img src={shop} alt="home logo" className="shopImg" />
+        <div className="svgContainerHome">
+          <img src={shop} alt="home logo" className="shopImg" />
+        </div>
         Accueil
       </button>
-      <button type="button"> Client </button>
-      <button type="button"> Partenaire Ext </button>
-      <button type="button"> Intervenant </button>
-      <button type="button"> Collaborateur </button>
+      <button type="button">
+        <div className="svgContainer">
+          <img src={client} alt="home logo" className="shopImg" />
+        </div>
+        Client
+      </button>
+      <button type="button">
+        <div className="svgContainer">
+          <img src={partenaire} alt="home logo" className="shopImg" />
+        </div>
+        Partenaire Ext
+      </button>
+      <button type="button">
+        <div className="svgContainer">
+          <img src={batiment} alt="home logo" className="shopImg" />
+        </div>
+        Intervenant
+      </button>
+      <button type="button">
+        <div className="svgContainer">
+          <img src={batiment} alt="home logo" className="shopImg" />
+        </div>
+        Collaborateur
+      </button>
     </SCardForm>
   );
 }
--- frontend/src/components/Sidebar/style.jsx
+++ frontend/src/components/Sidebar/style.jsx
@@ -1,24 +1,52 @@
 import styled from "styled-components";
 
 export default styled.aside`
-  border: solid yellowgreen 1px;
+  border-right: solid black 2px;
+  padding-right: 2rem;
   display: flex;
   flex-direction: column;
   width: 20vw;
   height: 100vh;
 
   button {
-    border: 3px solid #4a64d8;
     background-color: transparent;
     border-radius: 8px;
     box-sizing: border-box;
     width: 80%;
     align-self: center;
     margin-top: 1rem;
-    height: 2rem;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+
+  button:active {
+    background: #ffffff;
   }
 
   .shopImg {
-    background-color: rgba(45, 198, 173, 1);
+    box-sizing: border-box;
+    height: auto;
+    width: 1.8rem;
+  }
+  .svgContainer {
+    background-color: transparent;
+    border-radius: 8px;
+    box-sizing: border-box;
+    width: 15%;
+    height: auto;
+    margin-right: 1rem;
+  }
+  .svgContainerHome {
+    background: #2dc6ad;
+    box-shadow: 0px 4px 6px -1px rgba(0, 0, 0, 0.12),
+      0px 2px 4px -1px rgba(0, 0, 0, 0.07);
+    border-radius: 8px;
+    box-sizing: border-box;
+    width: 15%;
+    height: auto;
+    margin-right: 1rem;
   }
 `;
