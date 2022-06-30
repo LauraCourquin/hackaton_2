cc39b2ad249abf977e7c23abd5e45205bc43d464
[Lille-JS] Vaentin Nav <riverpure59@hotmail.fr>
2022-06-30 13:26:49 +0200

    update file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "@pages/Home";
 import Card from "@components/Card";
+import SideBar from "@components/Sidebar";
 
 export default function App() {
   return (
     <div>
       <Home />
+      <SideBar />
       <Card />
     </div>
   );
