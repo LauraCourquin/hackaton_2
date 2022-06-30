cff5302d474dd6b17025b4f14f8c8d83f160d7be
hamza benidar <benidar.hamza@gmail.com>
2022-06-30 10:12:31 +0200

    modif

--- frontend/src/components/Filter/index.jsx
+++ frontend/src/components/Filter/index.jsx
@@ -1,22 +1,49 @@
 import SFilter from "./style";
+// import axios from "axios";
 
 export default function Filter() {
   return (
+    // const filterData
+
+    //   const hSubmit = (evt) => {
+    //     evt.preventDefault();
+    //     axios
+    //       .post("http://localhost:5000/contact", formData)
+    //       .then(() => {
+    //         toast("Message envoyé!");
+    //       })
+    //       .catch(() => {
+    //         toast.error(
+    //           "Un problème est survenue, merci de réessayer ultérieurement "
+    //         );
+    //       });
+    //   };
+
     <SFilter>
       <div>
-        <h1>FILTRE</h1>
-        <input type="search" id="site-search" />
+        <section>
+          <h1>FILTRE</h1>
+          {/* <input type="search" id="site-search" />
+          {acteurs
+            .filter((acteur) => acteurs.type.includes("vibe"))
+            .map((acteurs) => (
+              <input
+                name={acteurs.job}
+                type="image"
+                src={acteurs.picture}
+                alt={acteurs.alt}
+                onClick={(event) => {
+                  filterData(event);
+                }}
+              />
+            ))} */}
+        </section>
       </div>
     </SFilter>
   );
 }
-
+// Récupérer du back : job, disponible
 // CREATE TABLE `acteur` (
-//   `id` int NOT NULL AUTO_INCREMENT,
-//   `firstname` varchar(45) NOT NULL,
-//   `lastname` varchar(45) NOT NULL,
-//   `picture` varchar(500) DEFAULT NULL,
-//   `skills` json DEFAULT NULL,
-//   `mail` varchar(255) NOT NULL,
 //   `jobs` varchar(45) DEFAULT NULL,
 //   `types` enum('client','collaborateur','prestateur externe','intervenant')
+// table project => in project
