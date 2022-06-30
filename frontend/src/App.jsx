6bd7514d2b045281e18b702cc3ba7adc1c0db59d
SDecambray <samir.decambray@gmail.com>
2022-06-30 16:01:11 +0200

    dd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import Home from "@pages/Home";
 
 export default function App() {
-<<<<<<< HEAD
   return (
     <div>
       <Home />
     </div>
   );
-=======
-  return <Home />;
->>>>>>> dev
 }
